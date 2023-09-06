3ba3d07d32a5bcfd47a2e758894a3eac716badbf
Alexey Zotov <alexey.zotov@ymail.com>
2023-09-07 00:50:38 +0300

    Generator Task 2 finished

--- src/main/java/ru/gb/StudentsApp/App.java
+++ src/main/java/ru/gb/StudentsApp/App.java
@@ -1,7 +1,11 @@
 package ru.gb.StudentsApp;
 
+import ru.gb.StudentsApp.Domen.Student;
 import ru.gb.StudentsApp.Domen.StudentGenerator;
+import ru.gb.StudentsApp.Domen.StudentGroup;
 import ru.gb.StudentsApp.Domen.StudentStream;
+
+import java.util.Arrays;
 
 public class App {
     public static void main(String[] args) {
@@ -10,8 +14,22 @@ public class App {
 
         StudentStream studentStream1 = studentGenerator.GetStudentStream();
         StudentStream studentStream2 = studentGenerator.GetStudentStream();
-        StudentStream studentStream3 = studentGenerator.GetStudentStream();
-        StudentStream studentStream4 = studentGenerator.GetStudentStream();
 
+        System.out.println("Task#1");
+        for (StudentGroup students : studentStream1) {
+            System.out.println("*".repeat(50));
+            for (Student student : students) {
+                System.out.println(student);
+            }
+        }
+
+        System.out.println("Task#2 - unsorted");
+        for (StudentGroup students : studentStream2) {
+            System.out.println("*".repeat(50));
+            for (Student student : students) {
+                System.out.println(student);
+            }
+
+        }
     }
 }
